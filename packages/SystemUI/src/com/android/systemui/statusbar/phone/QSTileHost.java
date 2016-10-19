94949dd0b6bb40304b402842e5c1fccebd50a2cd
Nico60 <ndhouailly@gmail.com>
2016-10-19 14:53:32 +0200

    [DUI] Navigation Bar Tile
    Change-Id: Ie3789c78a8bf04507d154c6588fa584ce8ccc925
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/aicp_strings.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -62,7 +62,11 @@ import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+<<<<<<< HEAD
 import com.android.systemui.qs.tiles.RebootTile;
+=======
+import com.android.systemui.qs.tiles.NavigationBarTile;
+>>>>>>> d99e930... [DUI] Navigation Bar Tile
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -473,7 +477,11 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("volume_panel")) return new VolumeTile(this);
+<<<<<<< HEAD
         else if (tileSpec.equals("key_disabler")) return new KeyDisablerTile(this);
+=======
+        else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
+>>>>>>> d99e930... [DUI] Navigation Bar Tile
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
